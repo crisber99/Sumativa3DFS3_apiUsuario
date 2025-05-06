789a076961c5c62494669a4d08f525d72195e978
Crisber99 <bcbs1986@gmail.com>
2025-05-05 20:44:16 -0400

    otro comentario

--- src/main/java/com/example/api_control_usuario/Rol/RolesController.java
+++ src/main/java/com/example/api_control_usuario/Rol/RolesController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/rol")
 @RequiredArgsConstructor
